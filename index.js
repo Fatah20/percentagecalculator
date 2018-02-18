65867db303517ead3226439dd9af7b7860ff6875
Socdaaldev <socdaaldev1@gmail.com>
2018-02-17 21:33:13 -0500

    Simple calculation test

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express =  require('express');
 var cors = require('cors');
+var $ = require("jquery");
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
+
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get('/', jsonParser, function(request, response) {
   response.send('Hello World!')
 });
 
@@ -17,6 +25,15 @@ app.get('/users', function(req, res) {
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
+});
+
+app.post('/calc', function(req, res){
+  if (!req.body) return res.sendStatus(400);
+  console.log("The Request body is: " + req.body[0]);
+  console.log("getting to the calc request");
+  var result = req.body[0]*req.body[1]
+  var numbers = {  total: result};
+  res.send(numbers)
 });
 
 app.listen(app.get('port'), function() {
