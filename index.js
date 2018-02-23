00148fd0654c09f07517bb18ceb3b91f73e3887a
Socdaaldev <socdaaldev1@gmail.com>
2018-02-22 23:57:48 -0500

    Three calculation complete, need cleanup and validation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express =  require('express');
+
+
+
+var express = require('express');
 var cors = require('cors');
 var $ = require("jquery");
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
+
 
 
 const app = express();
@@ -14,28 +18,55 @@ app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', jsonParser, function(request, response) {
+app.get('/', jsonParser, function (request, response) {
   response.send('Hello World!')
 });
 
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
-app.post('/calc', function(req, res){
+app.post('/whatis', function (req, res) {
   if (!req.body) return res.sendStatus(400);
-  console.log("The Request body is: " + req.body[0]);
-  console.log("getting to the calc request");
-  var result = req.body[0]*req.body[1]
-  var numbers = {  total: result};
+  var result = calculate(req, "/whatis");
+  //var result = req.body[0]*req.body[1]
+  var numbers = { total: result };
   res.send(numbers)
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.post('/iswhat', function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = calculate(req, "/iswhat");
+  //var result = req.body[0]*req.body[1]
+  var numbers = { total: result };
+  res.send(numbers)
 });
+
+app.post('/incDec', function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = calculate(req, "/incDec");
+  //var result = req.body[0]*req.body[1]
+  var numbers = { total: result };
+  res.send(numbers)
+});
+
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+function calculate(req, type) {
+  fnumber = req.body[0];
+  snumber = req.body[1];
+  if (type == "/whatis") {
+    result = (fnumber / 100) * snumber;
+    return result;
+  }
+  else if (type == "/iswhat") {
+    result = (fnumber / snumber) * 100;
+    return result;
+  }
+  else if (type == "/incDec") {
+    difference = (snumber - fnumber);
+      result = (difference / fnumber) * 100;
+      return (result);
+   
+   
+   
+  }
+}
